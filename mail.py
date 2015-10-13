8251f3f6f63b6275b1d86474676aca9de391338a
Anthony <anthonyfadale@gmail.com>
2015-10-12 18:00:00 -0700

    Initial Commit; Working email script

---
+++ mail.py
@@ -0,0 +1,39 @@
+import smtplib, ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
+
+# Sender Info
+SENDER = config.get('Sender', 'email')
+PASSWORD = config.get('Sender', 'password')
+
+# Receiver Info
+TO = config.get('Receiver', 'to')
+SUBJECT = config.get('Receiver', 'subject')
+TEXT = config.get('Receiver', 'text')
+
+# Carrier Info
+SERVER = config.get('Carrier', 'server')
+PORT = config.getint('Carrier', 'port')
+
+# Create connection to carrier service
+server = smtplib.SMTP(SERVER, PORT)
+server.ehlo()
+server.starttls()
+server.login(SENDER, PASSWORD)
+
+BODY = '\r\n'.join([
+    'To: %s' % TO,
+    'From: %s' % SENDER,
+    'Subject: %s' % SUBJECT,
+    '',
+    TEXT
+    ])
+
+try:
+    server.sendmail(SENDER, [TO], BODY)
+    print 'email sent'
+except:
+    print 'error sending email'
+
+server.quit()
